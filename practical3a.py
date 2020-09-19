487b23eea9cba8527656ee48c9f4775fe76fdc4c
ThomasDevyesh <67968577+ThomasDevyesh@users.noreply.github.com>
2020-09-19 23:54:07 +0530

    Add files via upload

---
+++ practical3a.py
@@ -0,0 +1,27 @@
+class stack:
+    def __init__(self):
+        self.stack = list()
+    def push(self,data):
+        if data not in self.stack:
+            self.stack.append(data)
+            return True
+        return False
+    def pop(self):
+        if len(self.stack) <= 0:
+            return("Stack is empty")
+        return self.stack.pop()
+    def size(self):
+        return len(self.stack)
+myStack = stack()
+print(myStack.push(6))
+print(myStack.push(2))
+print(myStack.push(5))
+print(myStack.push(4))
+
+print(myStack.size())
+
+print(myStack.pop())
+print(myStack.pop())
+print(myStack.pop())
+print(myStack.pop())
+print(myStack.size())
